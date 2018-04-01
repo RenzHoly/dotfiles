d414aea59be5ef8a6ab540125e61f8deb2b988b7
renzholy <renzholy@gmail.com>
2018-04-01 10:19:31 +0800

    update

--- archlinux-openbox/install.sh
+++ archlinux-openbox/install.sh
@@ -13,4 +13,6 @@ cd yaourt
 makepkg -si
 cd ..
 
-yaourt -S guake openbox rsync openssh docker fcitx-googlepinyin fcitx lxdm dunst profile-sync-daemon polybar openbox-themes mpv xarchiver shadowsocks-qt5 flameshot rofi nitrogen rofi-greenclip plank openbox-themes lxdm-themes pigz p7zip unrar fontweak xtrlock compton proxychains libinput-gestures gesture-manager-git gtk-engines noto-fonts-cjk noto-fonts noto-fonts-emoji ttf-fira-code ttf-monaco ttf-ubuntu-font-family pcmanfm archlinux-wallpaper gvfs-smb obconf oblogout
+yaourt -S guake openbox rsync openssh fcitx-googlepinyin fcitx-im lxdm dunst profile-sync-daemon polybar openbox-themes mpv xarchiver shadowsocks-qt5 flameshot rofi nitrogen rofi-greenclip plank openbox-themes lxdm-themes pigz p7zip unrar fontweak xtrlock compton proxychains gtk-engines noto-fonts-cjk noto-fonts noto-fonts-emoji ttf-fira-code ttf-monaco ttf-ubuntu-font-family pcmanfm archlinux-wallpaper gvfs-smb lxappearance-obconf lxappearance oblogout archlinux-wallpaper
+
+yaourt -S libinput-gestures gesture-manager-git
