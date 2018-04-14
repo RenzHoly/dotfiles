9936821bc1f65dd402234c07cb16547fb98c5f3e
renzholy <renzholy@gmail.com>
2018-04-15 00:25:48 +0800

    update

--- MacBookPro11,5_ArchLinux-Openbox/install.sh
+++ MacBookPro11,5_ArchLinux-Openbox/install.sh
@@ -15,34 +15,37 @@ makepkg -si
 cd ..
 
 # openbox
-sudo pacman -S lxdm openbox oblogout obmenu lxappearance-obconf lxappearance
+yaourt -S lxdm openbox oblogout obmenu lxappearance-obconf lxappearance
+
+# network
+yaourt -S network-manager-applet networkmanager networkmanager-openvpn
 
 # desktop
-sudo pacman -S dunst nitrogen rofi rofi-greenclip plank compton xtrlock polybar
+yaourt -S dunst nitrogen rofi rofi-greenclip plank compton xtrlock polybar
 
 # gui tools
-sudo pacman -S guake shadowsocks-qt5 flameshot fontweak pcmanfm xarchiver mpv chinese-calendar
+yaourt -S guake shadowsocks-qt5 flameshot fontweak pcmanfm xarchiver mpv chinese-calendar
 
 # input method
-sudo pacman -S qtwebkit-bin fcitx-sogoupinyin fcitx-im
+yaourt -S qtwebkit-bin fcitx-sogoupinyin fcitx-im
 
 # cli tools
-sudo pacman -S rsync openssh profile-sync-daemon pigz p7zip unrar proxychains mons gvfs-smb
+yaourt -S rsync openssh profile-sync-daemon pigz p7zip unrar proxychains mons gvfs-smb
 
 # audop
-sudo pacman -S pulseaudio pulseaudio-alsa pulseaudio-bluetooth pamixer pa-applet-git
+yaourt -S pulseaudio pulseaudio-alsa pulseaudio-bluetooth pamixer ponymix pa-applet-git
 
 # fonts
-sudo pacman -S noto-fonts-cjk noto-fonts noto-fonts-emoji ttf-fira-code ttf-monaco ttf-ubuntu-font-family
+yaourt -S noto-fonts-cjk noto-fonts noto-fonts-emoji ttf-fira-code ttf-monaco ttf-ubuntu-font-family
 
 # gesture
-sudo pacman -S libinput-gestures gesture-manager-git
+yaourt -S libinput-gestures gesture-manager-git
 
 # temperature
-sudo pacman -S cpupower thermald mbpfan-git
+yaourt -S cpupower thermald mbpfan-git
 
 # themes
-sudo pacman -S gtk-engines archlinux-wallpaper lxdm-themes openbox-themes
+yaourt -S gtk-engines archlinux-wallpaper lxdm-themes openbox-themes
 
 # keyboard brightness
-sudo pacman -S upower python-dbus dbus
+yaourt -S upower python-dbus dbus
