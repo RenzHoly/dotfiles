f0bcbb8db67498656d3a533fc5f2418b57cf37ba
renzholy <renzholy@gmail.com>
2018-04-18 15:20:57 +0800

    update

--- MacBookPro11,5_ArchLinux-Openbox/bin/monitor-hotplug.sh
+++ MacBookPro11,5_ArchLinux-Openbox/bin/monitor-hotplug.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 function connect() {
+	killall compton
 	mons -s
+	compton &
 }
 
 function disconnect() {
+	killall compton
 	mons -o
+	compton &
 }
 
 xrandr | grep -E "(DisplayPort|HDMI)-. connected" &> /dev/null && connect || disconnect
