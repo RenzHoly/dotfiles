107d562fae888e16852176109ad4bc7d6e42e28d
renzholy <renzholy@gmail.com>
2018-04-27 08:43:12 +0800

    remove plymouth

--- MacBookPro11,5_ArchLinux_Openbox/install.sh
+++ MacBookPro11,5_ArchLinux_Openbox/install.sh
@@ -49,6 +49,3 @@ yaourt -S hid-apple-patched-git-dkms upower python-dbus dbus
 
 # gesture
 yaourt -S libinput-gestures gesture-manager-git
-
-# bootsplash
-yaourt -S plymouth plymouth-theme-arch-logo
