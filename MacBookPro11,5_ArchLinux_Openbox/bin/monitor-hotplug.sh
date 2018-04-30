fa53423014cd20a4bdc76ff692b844d49c597fed
renzholy <renzholy@gmail.com>
2018-04-30 20:43:11 +0800

    update

--- MacBookPro11,5_ArchLinux_Openbox/bin/monitor-hotplug.sh
+++ MacBookPro11,5_ArchLinux_Openbox/bin/monitor-hotplug.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+displaynum=`ls /tmp/.X11-unix/* | sed s#/tmp/.X11-unix/X##`
+export DISPLAY=":$displaynum.0"
+export XAUTHORITY=$(ps -C Xorg -f --no-header | sed -n 's/.*-auth //; s/ -[^ ].*//; p')
+
 function connect() {
-	mons -s
+	killall -w compton & mons -s && compton &
 }
 
 function disconnect() {
-	mons -o
+	killall -w compton & mons -o && compton &
 }
 
-xrandr | grep -E "(DisplayPort|HDMI)-. connected" &> /dev/null && connect || disconnect
+sleep 1 && xrandr | grep -E "(DisplayPort|HDMI)-. connected" &> /dev/null && connect || disconnect
