d128a04811aba52a31ad31f6da0bfe7a23ecb20a
renzholy <renzholy@gmail.com>
2018-05-04 19:04:05 +0800

    update

--- MacBookPro11,5_ArchLinux_Openbox/install.sh
+++ MacBookPro11,5_ArchLinux_Openbox/install.sh
@@ -39,7 +39,7 @@ yaourt -S pulseaudio pulseaudio-alsa pulseaudio-bluetooth pamixer ponymix pa-app
 yaourt -S noto-fonts-cjk noto-fonts noto-fonts-emoji ttf-fira-code ttf-monaco ttf-ubuntu-font-family
 
 # performance tuning
-yaourt -S acpid pstate-frequency thermald irqbalance driconf
+yaourt -S pstate-frequency thermald irqbalance driconf intel-undervolt
 
 # themes
 yaourt -S gtk-engines archlinux-wallpaper lxdm-themes openbox-themes
