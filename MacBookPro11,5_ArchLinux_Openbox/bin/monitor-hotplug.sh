e8c260e5bd93b1e80afdd7df255304d66fa537d8
renzholy <renzholy@gmail.com>
2018-05-18 23:14:28 +0800

    update

--- MacBookPro11,5_ArchLinux_Openbox/bin/monitor-hotplug.sh
+++ MacBookPro11,5_ArchLinux_Openbox/bin/monitor-hotplug.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-displaynum=`ls /tmp/.X11-unix/* | sed s#/tmp/.X11-unix/X##`
-export DISPLAY=":$displaynum"
-export XAUTHORITY=$(ps -C Xorg -f --no-header | sed -n 's/.*-auth //; s/ -[^ ].*//; p')
-
 function connect() {
 	mons -s
 }
